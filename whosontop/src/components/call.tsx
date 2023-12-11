d41d0aef5f6c7da8aee8a49922f8f2b5cccb4f1b
blakestack <66808440+blakestack@users.noreply.github.com>
2023-12-10 21:01:09 -0500

    Change 'target' and 'module' values in the config file, Add boilerplate for API request

--- whosontop/src/components/call.tsx
+++ whosontop/src/components/call.tsx
@@ -1,3 +1,20 @@
+const cfbd = require('cfb.js');
+
 const key = process.env.CFBD_API_KEY
 
 const API_URL = "api.collegefootballdata.com/";
+
+const defaultClient = cfbd.ApiClient.instance;
+
+const ApiKeyAuth = defaultClient.authentications['ApiKeyAuth'];
+ApiKeyAuth.apiKey = `${key}`;
+ApiKeyAuth.apiKeyPrefix["Authorization"] = "Bearer";
+
+var api = new cfbd.GamesApi();
+
+let year = 2020;
+const games = await api.getGames(year);
+
+console.table(games);
+
+export { };
